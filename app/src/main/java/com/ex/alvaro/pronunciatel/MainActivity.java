ebbc8cbd760864a49df181f4d2dcbde5b775335c
Alvaro Mercado <alvaroawake@gmail.com>
2015-03-05 11:07:26 -0400

    Inicio de Interfaz inicial

--- app/src/main/java/com/ex/alvaro/pronunciatel/MainActivity.java
+++ app/src/main/java/com/ex/alvaro/pronunciatel/MainActivity.java
@@ -5,16 +5,27 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 import Clases.Usuario;
 
 
 public class MainActivity extends Activity {
 
+    //Elementos de la pantalla
+    Button btnActividades, btnPuntuacion, btnAyuda, btnUsuario;
+    TextView lblBienvenido;
+
+    String bienvenido;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cargarElementosInterfaz();
+        bienvenido=lblBienvenido.getText().toString();
 
         //Crea un usuario para cargar los valores existentes
         Usuario usu=new Usuario("",getApplicationContext());
@@ -25,12 +36,18 @@ public class MainActivity extends Activity {
             //Ingresar Nomber de usuario
         }else {
             //Continuar al menu
+            lblBienvenido.setText(bienvenido+" "+usu.getNombre());
         }
+    }
 
-
-
-
-
+    private void cargarElementosInterfaz() {
+        //Botones
+        btnActividades=(Button)findViewById(R.id.btnActividades);
+        btnPuntuacion=(Button)findViewById(R.id.btnPuntuacion);
+        btnUsuario=(Button)findViewById(R.id.btnUsuario);
+        btnAyuda=(Button)findViewById(R.id.btnAyuda);
+        //Labels - TextView
+        lblBienvenido=(TextView)findViewById(R.id.lblBienvenida);
     }
 
 }
