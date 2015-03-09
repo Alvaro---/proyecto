74da300d11450f58a61465aafd057b76a88cd10d
Alvaro Mercado <alvaroawake@gmail.com>
2015-03-09 09:16:17 -0400

    eee....

--- app/src/main/java/com/ex/alvaro/pronunciatel/MainActivity.java
+++ app/src/main/java/com/ex/alvaro/pronunciatel/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends Activity {
 
     String bienvenido;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
